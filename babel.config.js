77461dd797936987c09cd649bb31dee9a3f12cd0
yangyuwei <627888464@qq.com>
2019-09-20 09:46:04 +0800

    导入 Vant

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [// vant 依赖所需配置
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
   ]
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 按需注册 Vant 组件
+import { Button } from 'vant'
 
+Vue.use(Button) // 用Vue.use(xxx)的方法去全局注册组件
 Vue.config.productionTip = false
 
 new Vue({
