1a47f7e3d23202eb15556211ca6b151a7ba6cffa
yangyuwei <627888464@qq.com>
2019-09-20 10:06:53 +0800

    导入 Vant以及一些ren配置安装

--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 按需注册 Vant 组件
-import { Button } from 'vant'
+// // 按需注册 Vant组件
+// import { Button } from 'vant'
+// 按需注册 Vant 组件的组合写法
+import {
+  Button,
+  Cell,
+  CellGroup
+} from 'vant'
+// 按需注册 amfe-flexible组件（动态设置html标签的字体大小）
+import 'amfe-flexible/index.js'
+// Vue.use(Button) // 用Vue.use(xxx)的方法 全局注册组件
+// 用Vue.use(xxx)的方法 全局注册组件组合写法
+Vue
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)
 
-Vue.use(Button) // 用Vue.use(xxx)的方法去全局注册组件
 Vue.config.productionTip = false
 
 new Vue({
