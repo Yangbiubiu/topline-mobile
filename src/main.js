2135a4bde93444c85030443de14a1dc2ac413d0b
yangyuwei <627888464@qq.com>
2019-09-21 11:47:09 +0800

    创建登录组件并配置路由和页面布局及基本结构

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
 // // 按需注册 Vant组件
 // import { Button } from 'vant'
 // 按需注册 Vant 组件的组合写法
@@ -10,7 +11,9 @@ import {
   Cell,
   CellGroup,
   Tabbar,
-  TabbarItem
+  TabbarItem,
+  Field,
+  NavBar
 } from 'vant'
 // 按需注册 amfe-flexible组件（动态设置html标签的字体大小）
 import 'amfe-flexible/index.js'
@@ -22,6 +25,8 @@ Vue
   .use(CellGroup)
   .use(Tabbar)
   .use(TabbarItem)
+  .use(Field)
+  .use(NavBar)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Tabbar from '@/views/tabbar'
-Vue.use(VueRouter) // 用Vue.use(xxx)的方法去全局注册组件
+import Login from '@/views/login'
+// 用Vue.use(xxx)的方法去全局注册组件
+Vue.use(VueRouter)
 // 实例化路由对象
 const router = new VueRouter({
   // 配置路由表
   routes: [
     {
+      name: 'tabbar',
       path: '/tabbar',
       component: Tabbar
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
